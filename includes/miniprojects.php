81cd30a2ba81384ce22ed7d1f556380d7c510e88
Hagar Shilo <strayblues@gmail.com>
2016-12-29 15:01:11 +0200

    Remove Lotto project

--- includes/miniprojects.php
+++ includes/miniprojects.php
@@ -1,6 +1,6 @@
 <h1 class="display-4">My Work</h1>
 <div class="row row-projects">
-  <div class="col-md-4">
+  <div class="col-md-6">
     <div class="thumbnail thumbnail-mid">
       <div>
         <h3>Mandala</h3>
@@ -17,7 +17,7 @@
       </div>
     </div>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-6">
     <div class="thumbnail thumbnail-mid">
       <div>
         <h3>Quiz</h3>
@@ -31,23 +31,6 @@
       <div>
         <a href=""><i class="fa fa-android fa-4x" aria-hidden="true"></i></a>
         <a href="https://github.com/strayblues/android-js-quiz"><i class="fa fa-github fa-4x" aria-hidden="true"></i></a>
-      </div>
-    </div>
-  </div>
-  <div class="col-md-4">
-    <div class="thumbnail">
-      <div>
-        <h3>Lotto</h3>
-        <p class="app-description-white">
-          <?php
-            $p4=file_get_contents("paragraphs/lotto.txt");
-            echo $p4;
-          ?>
-        </p>
-      </div>
-      <div>
-        <a href=""><i class="fa fa-android fa-4x" aria-hidden="true"></i></a>
-        <a href=""><i class="fa fa-github fa-4x" aria-hidden="true"></i></a>
       </div>
     </div>
   </div>
