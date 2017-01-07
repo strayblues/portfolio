256ba018691308f40d391e2d80e948c498e2fe2e
Hagar Shilo <strayblues@gmail.com>
2017-01-07 19:58:42 +0200

    Update content and move unused files to trash

--- includes/skills.php
+++ includes/skills.php
@@ -7,18 +7,20 @@
   <div class="col-md-6">
     <h2>What I can do</h2>
     <ul>
-      <li>Android hybrid apps</li>
+      <li>Client side development</li>
+      <li>Android development</li>
+      <li>Responsive web design</li>
       <li>Single page applications</li>
-      <li>Responsive Web Pages</li>
-      <li>Browser extenstions<i class="fa fa-chrome" aria-hidden="true"></i></li>
+      <li>Browser extenstions</li>
     </ul>
   </div>
   <div class="col-md-6">
     <h2>Technical skills</h2>
       <ul>
         <li>HTML5 & CSS3</li>
-        <li>JavaScript: ES5, ES6, jQuery & Bootstrap</li>
-        <li>Angular 2 & TypeScript</li>
+        <li>JavaScript (ES5, ES6)</li>
+        <li>jQuery & Bootstrap</li>
+        <li>TypeScript & Angular 2</li>
         <li>Java, PHP & Python/django</li>
       </ul>
   </div>
