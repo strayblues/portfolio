ee84e8c95d21ac1ada811442c0d1dd169820461e
Hagar Shilo <strayblues@gmail.com>
2017-02-07 21:50:40 +0200

    Change the form's CSS a little and add new images for future use

--- includes/aboutyou.php
+++ includes/aboutyou.php
@@ -6,7 +6,7 @@
   // Our submit function
   function submitForm(){
 	_("send").disabled = true; // The user can't double click the btn
-  $(".status").html("Please wait...");
+  $(".status").html("<p>Please wait...</p>");
 //	_("status").innerHTML = 'Please wait...';
 	var formdata = new FormData(); // An object
 	formdata.append( "n", _("n").value ); // The value is what the use typed in
@@ -18,7 +18,7 @@
 		if(ajax.readyState == 4 && ajax.status == 200) {
 			if(ajax.responseText == "success"){
         // Confirm that the email was sent
-        $(".status").html('Thanks '+_("n").value+', your message has been sent.');
+        $(".status").html('<p>Thanks '+_("n").value+', your message has been sent.</p>');
         setTimeout(function(){_("send").disabled=false}, 2500);
 			} else { // In the event of data processing failure:
         // Output a msg about why the email wasn't sent
@@ -31,44 +31,45 @@
 	ajax.send( formdata );
 }
   </script>
-
-  <div class="row row-about about">
-    <div class="col-md-6">
-      <div class="modal-content">
-        <div class="contact-form form-horizontal">
-          <div class="modal-header">
-            <h1 class="modal-light display-4">About You</h1>
-            <h1>NOT READY YETTT</h1>
-            <form id="my_form" onsubmit="submitForm(); return false;">
-                <div class="modal-body">
-                  <div class="form-group">
-                    <div class="col-lg-12">
-                      <input type="text" id="n" placeholder="Your name" class="form-control" required>
-                    </div>
-                  </div><!--/form-group-->
-                  <div class="form-group">
-                    <div class="col-lg-12">
-                      <input type="email" id="e" placeholder="you@example.com" class="form-control" required>
-                    </div>
-                  </div><!--/form-group-->
-                  <div class="form-group">
-                    <div class="col-lg-12">
-                      <textarea type="text" rows="8" id="m" placeholder="Your message..." class="form-control" required></textarea>
-                    </div>
-                  </div><!--/form-group-->
-                </div>
+<div class="row row-about about">
+  <div class="col-md-6">
+    <div class="modal-content">
+      <div class="contact-form form-horizontal">
+        <div class="modal-header">
+          <h1 class="modal-light display-4">About You</h1>
+          <form id="my_form" onsubmit="submitForm(); return false;">
+              <div class="modal-body">
+                <div class="form-group">
+                  <div class="col-lg-12">
+                    <input type="text" id="n" placeholder="Your name" class="form-control" required>
+                  </div>
+                </div><!--/form-group-->
+                <div class="form-group">
+                  <div class="col-lg-12">
+                    <input type="email" id="e" placeholder="you@example.com" class="form-control" required>
+                  </div>
+                </div><!--/form-group-->
+                <div class="form-group">
+                  <div class="col-lg-12">
+                    <textarea type="text" rows="12" id="m" placeholder="Your message..." class="form-control" required></textarea>
+                  </div>
+                </div><!--/form-group-->
               </div>
-              <div class="modal-footer">
-                <button class="btn btn-primary btn-sm" id="send" type="submit"
-                value="Send" onclick="">Send</button>
-              </div>
-              <div class="status"></div>
-            </div><!--/form class-->
-        </form>
-
-      </div> <!-- /modal content -->
-    </div>
-    <div class="col-md-6">
-      <img src="images/unicorn_duckie.png" alt="Unicorn duckie" style="float: right; width: 90%;"></img>
-    </div>
+            </div>
+            <div class="modal-footer">
+              <button class="btn btn-primary btn-sm" id="send" type="submit"
+              value="Send">Send</button>
+            </div>
+            <div class="status"></div>
+          </div><!--/form class-->
+      </form>
+    </div> <!-- /modal content -->
   </div>
+
+  <div class="col-md-6">
+    <!--
+    <img class="" src="images/ma.png" alt="Mandala" style="float: right; width: 95%;"></img>
+  -->
+    <img src="images/unicorn_duckie.png" alt="Unicorn duckie" style="float: right; width: 90%;"></img>
+  </div>
+</div>
