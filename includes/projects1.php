756fe157b68ac7bb23988ea5ceb9467ad341f44a
Hagar Shilo <strayblues@gmail.com>
2017-02-10 19:45:54 +0200

    Stop window from refreshing when popup window opens

--- includes/projects1.php
+++ includes/projects1.php
@@ -5,7 +5,7 @@
       <div>
         <h3>Mandala</h3>
         <div class="first">
-          <a id="popup-mandala" href="#" href="https://strayblues.github.io/mandala/"><i class="fa fa-desktop fa-4x" aria-hidden="true"></i></a>
+          <a id="popup-mandala" href="#"><i class="fa fa-desktop fa-4x" aria-hidden="true"></i></a>
           <a href="https://github.com/strayblues/mandala"><i class="fa fa-github fa-4x" aria-hidden="true"></i></a>
         </div>
         <p class="app-description-white">
