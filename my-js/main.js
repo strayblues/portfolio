f41407f8c48b19074f3bca28642a3c04cbcdda32
Hagar Shilo <strayblues@gmail.com>
2017-02-16 03:11:11 +0200

    Open Mandala demo in wider window

--- my-js/main.js
+++ my-js/main.js
@@ -33,7 +33,7 @@ $('#popup-quiz').click(function(){
 
 // Popup for Mandala
 $('#popup-mandala').click(function(){
-  var myWindow = window.open("https://strayblues.github.io/mandala/", "mandala", "width=900,height=600");
+  var myWindow = window.open("https://strayblues.github.io/mandala/", "mandala", "width=960,height=540");
 });
 
 // Slide effect on Mandala
