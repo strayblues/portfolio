965aac6f7e7d5b217413cb056d3d61a490ada422
Hagar Shilo <strayblues@gmail.com>
2019-02-28 16:17:52 +0200

    Update AboutMe

--- includes/skills.php
+++ includes/skills.php
@@ -1,19 +1,8 @@
 <div class="row row-about about">
   <div class="col-md-9 aboutme-text">
     <p>
-      My name is Hagar Shilo.
-    </p><p>
-      I began to take an interest in web and mobile development in early 2016.
-    </p>
-<!--
-    <p>
-      I'm also into math-inspired art, painting, theoretical linguistics, inline skating, playing musical instruments, and even sewing...
-      but this is my web and mobile portfolio, so let's try and stay focused.
-    </p>
--->
-    <p>
-      I develop in JS and related technologies.
-      Here's what I know and what I can do:
+      Hi. My name is Hagar Shilo.
+      I am a frontend developer. I'm also into math-inspired art, painting, theoretical linguistics, inline skating, playing musical instruments, and even sewing. But this is my web portfolio, so let's try and stay on course.
     </p>
   </div>
   <div class="col-md-3">
@@ -24,19 +13,17 @@
   <div class="col-md-6">
     <h2>What I can do</h2>
     <ul>
+      <li>Stay on course</li>
       <li>Client side development</li>
-      <li>Hybrid app development</li>
       <li>Responsive web design</li>
-      <li>Single page applications</li>
     </ul>
   </div>
   <div class="col-md-6">
     <h2>Technical skills</h2>
       <ul>
         <li>HTML5, CSS3, JavaScript</li>
-        <li>jQuery, Bootstrap</li>
-        <li>React-Redux, Webpack, Babel, ES6</li>
-        <li>PHP web forms</li>
+        <li>jQuery, PHP</li>
+        <li>Vue.js, Bootstrap</li>
       </ul>
   </div>
 </div>
